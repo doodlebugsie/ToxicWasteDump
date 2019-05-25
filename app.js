9f21dc652c55e04bccb0254152e09efd6a433218
doodlebug <50970387+doodlebugsie@users.noreply.github.com>
2019-05-25 01:58:20 -0700

    new

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const navSlide = () => {
     //Link Animation
     .navlinks.forEach((link, index) => {
       if (link.style.animation) {
-        link.style.animation = '';
+          link.style.animation = '';
       } else {
           link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1}s';
       }
