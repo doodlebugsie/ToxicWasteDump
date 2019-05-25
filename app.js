1e7ac66713614cc09f3afd912b28f6eec979d686
doodlebug <50970387+doodlebugsie@users.noreply.github.com>
2019-05-25 00:10:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const navSlide = () => {
   const burger = document.querySelector ('.burger');
   const nav = document.querySelector('.navlinks');
-  const navlinks = document.querySelectorAll ('.navlinks li');
+  const navLinks = document.querySelectorAll ('.nav-links li');
   //Navigation Toggle
 
   burger.addEventListener ('click',()=>{
-    nav.classlist.toggle('nav-active');
+    nav.classList.toggle('nav-active');
   });
   //Link Animation
   .navlinks.forEach((link, index) => {
-    link.style.animation = 'navLinkFade 0.5s ease forwards %{index / 7}'
+    link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 2}s';
     console.log(index/7);
-
   });
 }
 
