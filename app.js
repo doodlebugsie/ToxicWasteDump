4ff6e2f02350c6f9ef67d9bef3782caf436a7c77
doodlebug <50970387+doodlebugsie@users.noreply.github.com>
2019-05-26 01:45:34 -0700

    w

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 const navSlide = () => {
-  const burger = document.querySelector ('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll ('.nav-links li');
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-  //set nav active status
-  let isActive = false;
-  console.log("nav-active: " + isActive)
-
-
-  burger.addEventListener ('click',()=>{
-      //Navigation Toggle
-    nav.classList.toggle('nav-active');
-    isActive = !isActive;
+    // Set nav active status with boolean
+    let isActive = false;
     console.log("nav-active : " + isActive)
 
-    //Link Animation
-    .navlinks.forEach((link, index) => {
-      if (link.style.animation) {
-          link.style.animation = '';
-      } else {
-          link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1}s';
-      }
-    });
-  });
-}
 
+    burger.addEventListener("click", ()=>{
+
+        //Toggle nav
+        nav.classList.toggle("nav-active");
+
+        // Toggle nav active status to true/false
+        isActive = !isActive;
+        console.log("nav-active : " + isActive)
+
+        //Animate link
+        navLinks.forEach((link, index)=>{
+            if(isActive) {
+                link.style.animation = `navLinkFadeIn 0.4s ease forwards ${index / 7 + 0.2}s`;
+                console.log("nav li: "+ index + " in");
+            } else {
+                // link.style.animation = "";
+                link.style.animation = `navLinkFadeOut 0.2s ease forwards 0s`;
+                // link.style.animation = "";
+                console.log("nav li: "+ index + " out");
+            }
+            console.log(link.style.animation);
+        });
+
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+
+}
 navSlide();
